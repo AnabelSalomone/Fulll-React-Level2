e1f9f522e0724dd211a4aa1fa7f2fd095e48a1ed
AnabelSalomone <anabel.salomone@gmail.com>
2021-05-23 18:29:29 +0200

    add switch cases to error file + documentation

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ import "./index.css";
 function App() {
   const [searchResult, setSearchResult] = useState([]); // established via the APIData function and sent to the SerachResults component
   const [keyword, setKeyword] = useState(""); // initializes the keyword
-  const [codeStatus, setCodeStatus] = useState(200);
+  const [codeStatus, setCodeStatus] = useState(204); // initializes code status
 
   const APIData = async (keyword) => {
     const url = `https://api.github.com/search/users?q=${keyword}`;
 
-    const data = await fetch(url);
-    setCodeStatus(data.status);
-    const json = await data.clone().json();
-    setSearchResult(json.items);
+    const data = await fetch(url); // fetches the data
+    setCodeStatus(data.status); // sets code status
+    const json = await data.json(); //stores data
+    setSearchResult(json.items); //sets the 'items' array to the serchResults var
   };
 
   const handleSearchbar = (keyword) => {
@@ -23,12 +23,12 @@ function App() {
   };
 
   const handleOnChange = (e) => {
-    setKeyword(e.target.value);
+    setKeyword(e.target.value); // updates the keyword var
   };
 
   useEffect(() => {
-    if (keyword.length > 0) {
-      handleSearchbar(keyword);
+    if (keyword.length > 0) { // avoids useEffect to be triggered on the first mount
+      handleSearchbar(keyword); // calls the API everytime 'keyword' changes
     }
   }, [keyword]);
 
--- src/components/Error.js
+++ src/components/Error.js
@@ -2,15 +2,33 @@ import React from "react";
 import "../index.css";
 
 const Error = ({ codeStatus }) => {
-  if(codeStatus === 403){
-      return (
-        <div>
-          <h3>Data is not available because query limit was attained, please try again in a few moments</h3>
-        </div>
-      );
+  let message = "";
+
+  switch (codeStatus) {
+    // If query limit is attained
+    case 403:
+      message =
+        "Data is not available because query limit was attained, please try again in a few moments";
+      break;
+    // If there are no results
+    case 200:
+      message = "No results";
+      break;
+    // When landing to the page
+    case 204:
+      message = "Please, start typing...";
+      break;
+
+    // Other possible errors
+    default:
+      message = "An error occurred";
   }
 
-  return <h3>No results.</h3>
+  return (
+    <div>
+      <h3>{message}</h3>
+    </div>
+  );
 };
 
 export default Error;
